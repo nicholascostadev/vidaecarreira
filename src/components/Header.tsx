11a51f699dd501405f8f6ddb193afdb5ac93b4c7
Nicholas <nicholascostadev@gmail.com>
2022-04-26 20:20:50 -0300

    FIX: Responsiveness on '/ajuda' page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,35 +55,36 @@ const Header = () => {
 					className="mr-5 mt-8 hover:cursor-pointer absolute right-0 top-0"
 					onClick={toggleMenu}
 				/>
-				<div className="md:hidden flex flex-col border-b-2 w-full pb-5">
+				<div className="md:hidden flex flex-col w-full pb-5">
 					<h1 className="text-3xl">Titulo</h1>
 				</div>
+				<hr />
 				<div className="md:hidden flex flex-col">
 					<a
 						href="#definicao"
 						onClick={toggleMenu}
-						className="py-5 hover:border-b-2 hover:border-gray-400 border-b-2 border-transparent transition-all"
+						className="py-5 hover:border-b-2 mt-4 text-center hover:border-gray-400 border-b-2 border-transparent transition-all"
 					>
 						O que é
 					</a>
 					<a
 						href="#diferenca"
 						onClick={toggleMenu}
-						className="py-5 hover:border-b-2 hover:border-gray-400 border-b-2 border-transparent transition-all"
+						className="py-5 hover:border-b-2 text-center hover:border-gray-400 border-b-2 border-transparent transition-all"
 					>
 						Tristeza ou Depressão?
 					</a>
 					<a
 						href="#sintomas"
 						onClick={toggleMenu}
-						className="py-5 hover:border-b-2 hover:border-gray-400 border-b-2 border-transparent transition-all"
+						className="py-5 hover:border-b-2 text-center hover:border-gray-400 border-b-2 border-transparent transition-all"
 					>
 						Sintomas
 					</a>
 					<Link
 						to="/ajuda"
 						onClick={toggleMenu}
-						className="my-5 lg:px-10 px-5 ml-5 py-2 rounded-full bg-green-500 hover:bg-green-600 text-white  "
+						className="my-5 text-center lg:px-10 px-5 ml-5 py-2 rounded-full bg-green-500 hover:bg-green-600 text-white  "
 					>
 						Ajuda
 					</Link>
--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -25,7 +25,7 @@ const Help = () => {
 		<div className="help h-screen">
 			<Header />
 			<div className="text-xl h-100 lg:px-20 px-5 pb-5">
-				<p className="mt-40 mb-1 ">
+				<p className="mt-40 mb-1 lg:text-xl md:text-lg text-sm  ">
 					A primeira indicação é procurar imediatamente um médico especializado
 					no assunto. Depressão é uma doença séria que deve ser tratada com uma
 					pessoa que realmente conhece do assunto e sabe como pode te ajudar.
@@ -34,66 +34,86 @@ const Help = () => {
 					existe o
 					<a
 						href="https://www.cvv.org.br/"
-						className="text-blue-500 hover:underline"
+						className="text-blue-500 hover:underline lg:text-xl md:text-lg text-sm"
 					>
 						CVV(Centro de Valorização da Vida)
 					</a>
 					que estará atendendo voluntariamente 24 horas por dia pelo telefone,
 					email e chat sob total sigilo.
 				</p>
-				<p>
+				<p className="lg:text-xl md:text-lg text-sm">
 					Ou ligue diretamente para o contato do CVV para o número
-					<span className="text-blue-500"> 198</span> ou contate-os por{' '}
+					<span className="text-blue-500 lg:text-xl md:text-lg text-sm">
+						{' '}
+						198
+					</span>{' '}
+					ou contate-os por{' '}
 					<a
 						href="https://www.cvv.org.br/e-mail/"
-						className="text-blue-500 hover:underline"
+						className="text-blue-500 hover:underline lg:text-lg md:text-lg text-sm"
 					>
 						email.
 					</a>
 				</p>
-				<p>
+				<p className="lg:text-xl md:text-lg text-sm">
 					Aqui têm alguns lugares de atendimento do CVV pessoalmente, caso
 					queira acessar todos os lugares, entre no
 					<a
 						href="https://www.cvv.org.br/"
 						target="__blank"
-						className="text-blue-500 hover:underline"
+						className="text-blue-500 hover:underline lg:text-xl md:text-lg text-sm"
 					>
 						{' '}
 						site
 					</a>{' '}
 					deles.
 				</p>
-				<select
-					className="border-2 rounded-md p-2 "
-					onChange={e => {
-						setSelectedOption(e.target.value);
-						console.log(e.target.value);
-					}}
-				>
-					{options.map(option => (
-						<option key={option} value={option}>
-							{option}
-						</option>
-					))}
-				</select>
-				<em className="ml-4 mr-1">(Estado - Cidade)</em>
-				<button
-					onClick={() => {
-						getPlaces();
-					}}
-					className="bg-green-500 hover:bg-green-600 ml-5 text-white font-bold py-2 px-4 rounded mt-10"
-				>
-					Encontrar Lugar
-				</button>
+				<div className="flex md:flex-row flex-col mt-10 items-center">
+					<select
+						className="border-2 rounded-md p-2 max-h-12 lg:text-xl md:text-lg text-sm"
+						onChange={e => {
+							setSelectedOption(e.target.value);
+							console.log(e.target.value);
+						}}
+					>
+						{options.map(option => (
+							<option
+								key={option}
+								value={option}
+								className="lg:text-xl md:text-lg text-sm"
+							>
+								{option}
+							</option>
+						))}
+					</select>
+					<em className="ml-4 mr-1 lg:text-lg md:text-sm text-xs md:mt-0 px-2 mt-2">
+						(Estado - Cidade)
+					</em>
+					<button
+						onClick={() => {
+							getPlaces();
+						}}
+						className="bg-green-500 hover:bg-green-600 ml-5 text-white font-bold py-2 px-4 rounded lg:text-xl md:text-lg text-sm text-center md:mt-0 mt-2"
+					>
+						Encontrar Lugar
+					</button>
+				</div>
 				<div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
 					{place.map((item: any) => {
 						return (
-							<div className="border-2 rounded-xl mt-5 p-5 drop-shadow-lg">
-								<p>Local: {item.title}</p>
-								<p>Endereço: {item.address}</p>
-								<p>Número de telefone: {item.phone}</p>
-								<p>Atendimento de {item.time}</p>
+							<div className="border-2 rounded-xl mt-5 p-5 drop-shadow-lg ">
+								<p className="lg:text-xl md:text-lg text-sm">
+									Local: {item.title}
+								</p>
+								<p className="lg:text-xl md:text-lg text-sm">
+									Endereço: {item.address}
+								</p>
+								<p className="lg:text-xl md:text-lg text-sm">
+									Número de telefone: {item.phone}
+								</p>
+								<p className="lg:text-xl md:text-lg text-sm">
+									Atendimento de {item.time}
+								</p>
 							</div>
 						);
 					})}
